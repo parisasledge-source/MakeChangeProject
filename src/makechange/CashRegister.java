06a20340c9081bbd0bc6d4aa4f41abb58fac2857
parisasledge-source <parisasledge@me.com>
2022-02-20 08:47:16 -0500

    display the number of bills and coins in method

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -37,12 +37,11 @@ public class CashRegister {
 		}
 		
 //		If the amount tendered is more than the cost of the item, 
-//		display the number of bills and coins that should be given to the customer.
 		else {
-			double change = amountPaid - price;
-			System.out.println("The money that should be given to the customer is $" + change);
+			//double change = amountPaid - price;
+			//System.out.println("The money that should be given to the customer is $" + change);
 			
-//			Call the method to calculate the change in the form of bills and coins .
+//			Call the method to calculate & display the number of bills and coins that should be given to the customer.
 			calculateChange(amountPaid, price);
 		}
 		
@@ -50,6 +49,53 @@ public class CashRegister {
 	}
 
 	public static void calculateChange(double amountPaid, double price) {
+		
+		double change = amountPaid - price;
+		
+		
+		double divisor1 = change / 20;
+		int twentyDollarBill = (int) divisor1;
+		double remainer1 = change % 20;
+		
+		
+		double divisor2 = remainer1 / 10;
+		int tenDollarBill = (int) divisor2;
+		double remainer2 = remainer1 % 10;
+		
+		
+		double divisor3 = remainer2 / 5;
+		int fiveDollarBill = (int) divisor3;
+		double remainer3 = remainer2 % 5;
+		
+		
+		double divisor4 = remainer3 / 1;
+		int oneDollarBill = (int) divisor4;
+		double remainer4 = remainer3 % 1;
+		
+		
+		double divisor5 = remainer4 / 0.25;
+		int quarter = (int) divisor5;
+		double remainer5 = remainer4 % 0.25;
+		
+		
+		double divisor6 = remainer5 / 0.1;
+		int dime = (int) divisor6;
+		double remainer6 = remainer5 % 0.1;
+		
+		
+		double divisor7 = remainer6 / 0.05;
+		int nickel = (int) divisor7;
+		double remainer7 = remainer6 % 0.05;
+		
+		
+		double divisor8 = remainer7 / 0.01;
+		int penny = (int) divisor8 + 1;
+		//double remainer8 = remainer7 % 0.01;
+		
+		System.out.println(twentyDollarBill + " Twenty Dollar Bill, " + tenDollarBill + " Ten Dollar Bill, " + fiveDollarBill + " Five Dollar Bill, " 
+		+ oneDollarBill + " One Dollar Bill, " + quarter + " quarter, " + dime + " dime, " + nickel + " nickel, " + penny + " penny.");
+		
+			
 	
 	}
 
